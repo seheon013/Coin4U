114a86b4ce8ad7d1b3070b02a11f340c77cff0eb
BootCamp_Sean <seheon013@gmail.com>
2022-06-04 13:23:55 -0700

    finally solved CORS problem

--- Develop/homepage.js
+++ Develop/homepage.js
@@ -1,31 +1,36 @@
 // querySelect user input from search bar
-var searchBarInput = document.querySelector('#searchBar')
+var searchBarInput = document.getElementById('searchBar')
     // user input to be stored locally
 
 // addEventListener to search button and submit user input to fetch
-searchBarInput.addEventListener('submit', searchApi);
+searchBarInput.addEventListener('submit', searchApi());
     // and load function fetch url 
-    function searchApi(query, format) {
-        var coinUrl = 'https://pro.coinmarketcap.com/account#';
+    function searchApi() {
+        console.log('d')
+        var coinUrl = 'https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
         
         fetch (coinUrl, {
             method: 'GET',
             headers: {
-                "X-Auth-Token": "dfa7d3fb-bc85-4ce8-8b1f-6d92e49f4c3e"
-            }
-    })
+                'X-CMC_PRO_API_KEY': 'e186bef8-4358-444a-b7d1-c798f20e09ff',
+            },
+        
+            
+        })
         // if fetch success then get response
             .then(function (response) {
                 if(response.ok) {
-                response.json().then(function(data) {
+                response.json().then(function(data){
                     console.log(data)
-                }) 
+                })}
                 // else alert error message
-                else(
+                else{
                     alert('Error' + response.statusText)
-                );    
-            }})
+                };    
+            })
         };
+
+
         // API key
 
 // define search params of response from url
