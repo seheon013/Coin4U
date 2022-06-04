972df5073958ca9add8ee8f9c3e98f33237eaada
aleena <aleenajohns31@gmail.com>
2022-06-04 13:43:05 -0700

    update files

--- Develop/homepage.js
+++ Develop/homepage.js
@@ -1,31 +1,29 @@
 // querySelect user input from search bar
-var searchBarInput = document.querySelector('#searchBar')
-    // user input to be stored locally
+var searchBarInput = document.getElementById('searchBar');
+   // user input to be stored locally
 
-// addEventListener to search button and submit user input to fetch
-searchBarInput.addEventListener('submit', searchApi);
-    // and load function fetch url 
-    function searchApi(query, format) {
-        var coinUrl = 'https://pro.coinmarketcap.com/account#';
-        
-        fetch (coinUrl, {
-            method: 'GET',
-            headers: {
-                "X-Auth-Token"; "dfa7d3fb-bc85-4ce8-8b1f-6d92e49f4c3e"
-            }
-        })
-        // if fetch success then get response
-            .then(function (response) {
-                if(response.ok) {
-                response.json().then (function(data) {
-                    console.log(data)}
-                );
-                // else alert error message
-                else(
-                    alert('Error' + response.statusText)
-                )};    
-            });
-        };
-        // API key
+//addEventListener to search button and submit user input to fetch
+searchBarInput.addEventListener('submit', searchApi());
+   // load function fetch url
+   function searchApi() {
+       console.log('d');
+       var exchangeUrl = 'https://v6.exchangerate-api.com/v6/de9b9fda136b7ee1b28581d7/latest/USD';
 
-// define search params of response from url
+       fetch (exchangeUrl, {
+           method: 'GET', 
+           headers: {
+               'X-CMC_PRO_API_KEY': 'de9b9fda136b7ee1b28581d7',
+           },
+       })
+    // if fetch success then get response
+       .then(function(response) {
+           if(response.ok) {
+               response.json().then(function(data){
+                   console.log(data)
+               })}
+               // else alert error message
+               else {
+                   alert('Error' + response.statusText)
+               };
+       })
+   };
