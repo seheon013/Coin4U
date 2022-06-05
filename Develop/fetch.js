bb1201c7e9cdbe05fc497577f6a7c33ce78e7d73
min-hinthar <102896221+min-hinthar@users.noreply.github.com>
2022-06-05 02:02:05 -0700

    fetch.js function refactored to omit credentials when requeting, returing SUCCESS 100 Arrays, homepage.html properly indented, stylesheet.css updated with linear gradients colors, emoji animation inspiration added, homepage.js code debugged

--- Develop/fetch.js
+++ Develop/fetch.js
@@ -1,5 +1,10 @@
+// Please feel free to use, edit, comment. Thanks --Min
 // https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
 // omit credentials to avoid exposing key api when fetching
+var searchBarInput = document.getElementById('searchBar')
+var submitBtnEl = document.getElementById('submitBtn')
+
+submitBtnEl.addEventListener('submit', searchApi());
 function searchApi() {
     console.log('d')
     var coinUrl = 'https://cors-anywhere.herokuapp.com/https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest';
@@ -9,10 +14,6 @@ function searchApi() {
         headers: {
             'X-CMC_PRO_API_KEY': 'e186bef8-4358-444a-b7d1-c798f20e09ff',
         },
-        mode: 'cors', //default
-        redirect: 'follow',
-        referrerPolicy: 'cross-origin',
-        body: JSON.stringify(data),
         credentials: 'omit', //user credentials omitted when requesting
     })
     // if fetch success then get response
