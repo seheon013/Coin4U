3ed00df115c30a7fa2a293ee4ff35716d23a508b
aleena <aleenajohns31@gmail.com>
2022-06-07 21:23:42 -0700

    coin card and exchange card added

---
+++ Develop/userpage.js
@@ -0,0 +1,30 @@
+var exchangeRate = 'https://v6.exchangerate-api.com/v6/de9b9fda136b7ee1b28581d7/latest/USD';
+
+        fetch (exchangeRate, {
+            method: 'GET', 
+            
+        })
+        .then(function (response) {
+            if(response.ok) {
+                response.json().then(function(data){
+                    console.log(data)
+
+                })}
+                else {
+                    alert('Error' + response.statusText)
+
+                };
+        })
+// function that renders data into HTML
+
+function displayCurrency(data) {
+    const myJSON = data;
+    const myJSONparse = JSON.parse(myJSON);
+    let USD = myJSONparse.USD;
+   console.log(USD);
+//    const USD = conversion_rates.USD;
+//    console.log(data.conversion_rates[0]);
+   const USDdiv = document.getElementById("currency-table");
+}
+
+ 
